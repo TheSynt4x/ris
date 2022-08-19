a16ede7f0d44f2fc8f4624add32bf325f5ba0a84
TheSynt4x <anonymousehaxor@gmail.com>
2022-08-19 23:14:33 +0200

    improve webui server

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 import asyncio
 import json
+import os
+from pathlib import Path
 
-import pymysql
 import websockets
 
 from app import libs, loader
 from app.core import logger, settings
 
-
-from pathlib import Path
+import time
 
 
 async def send(websocket, type, message):
@@ -19,35 +19,47 @@ async def handler(websocket):
     async for message in websocket:
         # todo: implement handlers
 
-        if message == "init":
-            logger.info(f"sending settings: {settings.subreddits}")
+        recv = json.loads(message)
+
+        logger.info(recv)
+
+        if "command" not in recv:
+            raise Exception("command not in payload")
+
+        # todo: add enums for each command
+        if recv["command"] == "init":
             await send(websocket, "subreddits", settings.subreddits)
             await send(websocket, "categories", settings.categories)
             await send(websocket, "global_post_limit", settings.global_post_limit)
+        elif recv["command"] == "get_content":
+            images = []
 
-        elif message == "get_content":
-            if settings.db_conn:
-                # todo: implement message arguments, so message can send limit={x}
-                images = await libs.db.get(
-                    "submissions", limit=392, cursor_cls=pymysql.cursors.DictCursor
+            format = None
+            if "format" in recv["params"]:
+                format = recv["params"]["format"]
+
+            for path in Path("assets").rglob("**/*"):
+                if len(path.name.split(".")) == 1:
+                    continue
+
+                image_id, image_format = path.name.split(".")
+
+                if format and format != image_format:
+                    continue
+
+                images.append(
+                    {
+                        "id": image_id,
+                        "url": str(path.absolute()),
+                        "format": image_format,
+                        "modified_at": time.ctime(os.path.getmtime(path)),
+                    }
                 )
-            else:
-                images = []
 
-                # todo: move to utils, implement limit={x} logic
-                for path in Path("assets").rglob("*"):
-                    images.append({"url": str(path.absolute())})
+            if "limit" in recv["params"]:
+                images = images[: recv["params"]["limit"]]
 
             await send(websocket, "submissions", images)
-
-        elif message == "add_subreddit":
-            pass
-
-        elif message == "add_category":
-            pass
-
-        elif message == "run":
-            pass
 
 
 async def main():
